dad23386d753763040c47cf882534a94357f12e7
Hongbo Liu <hbliu@freewheel.tv>
2018-06-06 12:53:04 +0800

    use long option ``--area`

--- geektime_ebook_maker/__init__.py
+++ geektime_ebook_maker/__init__.py
@@ -69,7 +69,7 @@ positional arguments:
 short options:
       -u            account
       -p            password
-      -a            area/country code
+      --area        area/country code
       -c            output directory of the ebook""")
 
 
@@ -103,13 +103,13 @@ def geektime():
     if len(args) == 0 or args[0] not in ('query', 'usage', 'ebook'):
         return usage()
 
-    options, _ = getopt.getopt(args[1:], "h?u:p:o:c:a:")
+    options, _ = getopt.getopt(args[1:], "h?u:p:o:c:", ['area='])
     opt_dict = dict(options)
     account = opt_dict.get('-u')
     password = opt_dict.get('-p')
     output_dir = opt_dict.get('-o')
     column_id = opt_dict.get('-c')
-    area_code = opt_dict.get('-a')
+    area_code = opt_dict.get('--area')
 
     os.environ["ACCOUNT"] = str(account)
     os.environ["PASSWORD"] = str(password)
