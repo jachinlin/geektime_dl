7707e584c1572906910a27e6fc26681f14c5c2e1
jachinlin <jachin1992@hotmail.com>
2018-11-21 02:05:26 +0800

    add m3u8 downloader

---
+++ geektime_ebook_maker/m3u8_downloader.py
@@ -0,0 +1,103 @@
+# coding: utf-8
+
+from gevent import monkey
+
+monkey.patch_all()
+from gevent.pool import Pool
+import gevent
+import requests
+from urllib.parse import urljoin
+import os
+import time
+
+
+class Downloader:
+    def __init__(self, pool_size, retry=3):
+        self.pool = Pool(pool_size)
+        self.session = self._get_http_session(pool_size, pool_size, retry)
+        self.retry = retry
+        self.dir = ''
+        self.file_name = 'out'
+        self.succed = {}
+        self.failed = []
+        self.ts_total = 0
+
+    def _get_http_session(self, pool_connections, pool_maxsize, max_retries):
+        session = requests.Session()
+        adapter = requests.adapters.HTTPAdapter(pool_connections=pool_connections, pool_maxsize=pool_maxsize,
+                                                max_retries=max_retries)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        return session
+
+    def run(self, m3u8_url, dir='', file_name=None):
+        self.dir = dir
+        self.file_name = file_name or self.file_name
+        if self.dir and not os.path.isdir(self.dir):
+            os.makedirs(self.dir)
+
+        r = self.session.get(m3u8_url, timeout=10)
+        if r.ok:
+            body = r.content.decode()
+            if body:
+                ts_list = [urljoin(m3u8_url, n.strip()) for n in body.split('\n') if
+                           n and not n.startswith("#")]
+                ts_list = list(zip(ts_list, [n for n in range(len(ts_list))]))
+                if ts_list:
+                    self.ts_total = len(ts_list)
+                    print(self.ts_total)
+                    g1 = gevent.spawn(self._join_file)
+                    self._download(ts_list)
+                    g1.join()
+        else:
+            print(r.status_code)
+
+    def _download(self, ts_list):
+        self.pool.map(self._worker, ts_list)
+        if self.failed:
+            ts_list = self.failed
+            self.failed = []
+            self._download(ts_list)
+
+    def _worker(self, ts_tuple):
+        url = ts_tuple[0]
+        index = ts_tuple[1]
+        retry = self.retry
+        while retry:
+            try:
+                r = self.session.get(url, timeout=20)
+                if r.ok:
+                    file_name = url.split('/')[-1].split('?')[0]
+                    print(file_name)
+                    with open(os.path.join(self.dir, file_name), 'wb') as f:
+                        f.write(r.content)
+                    self.succed[index] = file_name
+                    return
+            except:
+                retry -= 1
+        print('[FAIL]%s' % url)
+        self.failed.append((url, index))
+
+    def _join_file(self):
+        index = 0
+        outfile = ''
+        while index < self.ts_total:
+            file_name = self.succed.get(index, '')
+            if file_name:
+                infile = open(os.path.join(self.dir, file_name), 'rb')
+                if not outfile:
+                    outfile = open(os.path.join(self.dir, self.file_name + '.' + file_name.split('.')[-1]),
+                                   'wb')
+                outfile.write(infile.read())
+                infile.close()
+                os.remove(os.path.join(self.dir, file_name))
+                index += 1
+            else:
+                time.sleep(1)
+        if outfile:
+            outfile.close()
+
+
+if __name__ == '__main__':
+    downloader = Downloader(50)
+    downloader.run('https://res001.geekbang.org/media/video/17/ae/17af9f0d61ff9df13b26f299082d81ae/sd/sd.m3u8', '.')
